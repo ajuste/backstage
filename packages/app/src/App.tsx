1851cdf7a4a20edc589fd24376280c6e3b2f0747
Alvaro Juste <juste.alvaro@gmail.com>
2022-06-10 20:32:28 -0400

    Remove sign in page

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Navigate, Route } from 'react-router';
 import { apiDocsPlugin, ApiExplorerPage } from '@backstage/plugin-api-docs';
-import { githubAuthApiRef } from '@backstage/core-plugin-api';
-import { SignInPage } from '@backstage/core-components';
+
 import {
   CatalogEntityPage,
   CatalogIndexPage,
@@ -38,20 +37,6 @@ import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/
 
 const app = createApp({
   apis,
-  components: {
-    SignInPage: props => (
-      <SignInPage
-        {...props}
-        auto
-        provider={{
-          id: 'github-auth-provider',
-          title: 'GitHub',
-          message: 'Sign in using GitHub',
-          apiRef: githubAuthApiRef,
-        }}
-      />
-    ),
-  },
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
