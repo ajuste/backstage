1451598d570c0c02fed417b6a789db8d19f0675d
Chris Raborg <craborg@zerofox.com>
2023-05-04 09:16:14 -0400

    remove hashi-ui links from toolbox since it is disabled
    Ref: ZFE-68025

--- packages/app/src/components/toolkit/Tools.tsx
+++ packages/app/src/components/toolkit/Tools.tsx
@@ -4,9 +4,8 @@ import Drone from './Drone';
 import Consul from './Consul';
 import Vault from './Vault';
 import Nomad from './Nomad';
-import HashiUI from './HashiUI';
 import Architecture from './Architecture';
 import Prometheus from './Prometheus';
 import Grafana from './Grafana';
 
-export const getAllTools = () => [Okta, Jenkins, Drone, Consul, Vault, Nomad, HashiUI, Architecture, Prometheus, Grafana];
+export const getAllTools = () => [Okta, Jenkins, Drone, Consul, Vault, Nomad, Architecture, Prometheus, Grafana];
