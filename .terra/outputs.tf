d279098c6035ceecb65c82d99bb6a0db54dc1673
Luis Jury <ljury@zerofox.com>
2024-01-09 10:04:59 -0300

    Adds pillars to backstage tf outputs (#130)
    Refs: https://zf.atlassian.net/browse/ZFE-83190

---
+++ .terra/outputs.tf
@@ -0,0 +1,4 @@
+output "available_pillars" {
+  description = "List of all the Zerofox pillars"
+  value       = ["Attack Surface", "Data Science", "Disruption", "Intelligence", "Protection", "Response", "Sustaining"]
+}
