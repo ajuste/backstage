b141d3f98ee7e6cc4f1f3281a2859f69d9ba4f78
Alvaro Juste <juste.alvaro@gmail.com>
2024-10-14 08:07:40 -0300

    Add more tools to homepage

--- packages/app/src/components/toolkit/Tools.tsx
+++ packages/app/src/components/toolkit/Tools.tsx
@@ -7,5 +7,9 @@ import Nomad from './Nomad';
 import Architecture from './Architecture';
 import Prometheus from './Prometheus';
 import Grafana from './Grafana';
+import Superset from './Superset';
+import OpenMetadata from './OpenMetadata';
+import Airflow from './Airflow';
+import Metaflow from './Metaflow';
 
-export const getAllTools = () => [Okta, Jenkins, Drone, Consul, Vault, Nomad, Architecture, Prometheus, Grafana];
+export const getAllTools = () => [Okta, Jenkins, Drone, Consul, Vault, Nomad, Architecture, Prometheus, Grafana, Superset, OpenMetadata, Airflow, Metaflow];
