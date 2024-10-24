e41747f9650805f702f5b8bd2963d1d398bf3691
Alvaro Juste <juste.alvaro@gmail.com>
2024-10-24 08:00:31 -0300

    Add ingestion bq to sql golden path
    Refs: https://zf.atlassian.net/browse/ZFE-97200

--- packages/backend/src/plugins/scaffolderActions/ingestions.ts
+++ packages/backend/src/plugins/scaffolderActions/ingestions.ts
@@ -150,8 +150,8 @@ const createDagFolderFromTemplate = (_: ScaffolderActionFactoryOptions) => {
                 "ingestion/ingestion.template": `ingestion/${snakeCaseTaskName}.py`,
                 "ingestion/ingestion_factory.template": `ingestion/${snakeCaseTaskName}_factory.py`,
                 "task/task.template": `task/${snakeCaseTaskName}.py`,
-                "sql/task.sql": `sql/${snakeCaseTaskName}.sql`,
-                "ddl/task.sql": `ddl/${snakeCaseTaskName}.sql`,
+                "sql/task.sql.template": `sql/${snakeCaseTaskName}.sql`,
+                "ddl/task.sql.template": `ddl/${snakeCaseTaskName}.sql`,
             };
 
             // Rename files
@@ -185,6 +185,7 @@ const generatePullRequestTextsForDataLine = (_: ScaffolderActionFactoryOptions) 
             const { githubAuthor, jiraTicket, ingestionName, additionalNotes } = ctx.input;
 
             const description = `
+<<<<<<< HEAD
                 ## Overview
                 This PR introduces a new data ingestion DAG: \`${ingestionName}\`.
 
@@ -204,6 +205,28 @@ const generatePullRequestTextsForDataLine = (_: ScaffolderActionFactoryOptions) 
                 This PR should be reviewed by the Data Engineering Team. Please make sure everything written makes sense or update the commit if needed. Ask for help if you need it.
 
                 Please review and merge when ready.
+=======
+![image](https://github.com/user-attachments/assets/b377b39f-d949-4944-9afe-4fccb237b1a6)
+## Overview
+This PR introduces a new data ingestion DAG: \`${ingestionName}\`.
+
+## Reminder
+Please ensure to update the queries with \`{start_date}\` and \`{end_date}\`.
+
+## Additional Notes
+${additionalNotes || 'N/A'}
+
+## Author
+The author of this change is @${githubAuthor}.
+
+## JIRA Ticket
+${jiraTicket}
+
+## Review
+This PR should be reviewed by the Data Engineering Team. Please make sure everything written makes sense or update the commit if needed. Ask for help if you need it.
+
+Please review and merge when ready.
+>>>>>>> e24cd53728 (Add ingestion bq to sql golden path)
             `;
 
             const commitMessage = `feat(dags/data_lines/bq_to_sql/monolith): add ${ingestionName} DAG\nRefs: ${jiraTicket}`;
